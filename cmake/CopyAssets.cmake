dd7194a13cdb5d95c4bae0a58174cfb2575ba8c3
Tuan Nguyen <tufourn@gmail.com>
2025-02-20 17:42:04 -0500

    fix: assets copying

--- cmake/CopyAssets.cmake
+++ cmake/CopyAssets.cmake
@@ -1,10 +1,29 @@
-function(copy_assets TargetName DirToCopy)
+function(copy_assets TargetName)
     get_target_property(TargetSourceDir ${TargetName} SOURCE_DIR)
-    add_custom_command(
-            TARGET ${TargetName} POST_BUILD
-            COMMAND
-            ${CMAKE_COMMAND} -E copy_directory_if_different
-            ${TargetSourceDir}/${DirToCopy}
-            $<TARGET_FILE_DIR:${TargetName}>/${DirToCopy}
+    get_target_property(TargetBinaryDir ${TargetName} BINARY_DIR)
+
+    set(CopyCommands)
+    foreach (DirToCopy IN LISTS ARGN)
+        # copy from source to build
+        list(APPEND CopyCommands
+                COMMAND ${CMAKE_COMMAND} -E copy_directory
+                ${TargetSourceDir}/${DirToCopy}
+                $<TARGET_FILE_DIR:${TargetName}>/${DirToCopy}
+        )
+
+        # copy from build to source
+        list(APPEND CopyCommands
+                COMMAND ${CMAKE_COMMAND} -E copy_directory
+                $<TARGET_FILE_DIR:${TargetName}>/${DirToCopy}
+                ${TargetSourceDir}/${DirToCopy}
+        )
+
+    endforeach ()
+
+    add_custom_target(${TargetName}_copy_assets ALL
+            ${CopyCommands}
+            COMMENT "syncing assets for ${TargetName}"
     )
+
+    add_dependencies(${TargetName} ${TargetName}_copy_assets)
 endfunction(copy_assets)
